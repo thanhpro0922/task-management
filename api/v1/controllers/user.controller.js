32e9cb2ab3468054a052b82fcd2759f9dcd49604
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-12 06:24:45 +0700

    2.3 Màn hình reset mật khẩu

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -150,3 +150,34 @@ module.exports.otpPassword = async (req, res) => {
         token: token,
     });
 };
+
+//@ [POST] /api/v1/users/password/reset
+module.exports.resetPassword = async (req, res) => {
+    const token = req.body.token;
+    const password = req.body.password;
+
+    const user = await User.findOne({
+        token: token,
+    });
+
+    if (md5(password) === user.password) {
+        res.json({
+            code: 400,
+            message: "Mật khẩu mới không được trùng với mật khẩu cũ!",
+        });
+        return;
+    }
+
+    await User.updateOne(
+        {
+            token: token,
+        },
+        {
+            password: md5(password),
+        }
+    );
+    res.json({
+        code: 200,
+        message: "Đổi mật khẩu thành công!",
+    });
+};
