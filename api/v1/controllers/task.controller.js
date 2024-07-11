20e633242a7ba27c22a2d8452d1e1a382e17a3aa
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-12 04:27:53 +0700

    8. Xóa nhiều công việc

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -98,7 +98,21 @@ module.exports.changeMulti = async (req, res) => {
                     message: "Cập nhật trạng thái thành công!",
                 });
                 break;
-
+            case "delete":
+                await Task.updateMany(
+                    {
+                        _id: { $in: ids },
+                    },
+                    {
+                        deleted: true,
+                        deletedAt: new Date(),
+                    }
+                );
+                res.json({
+                    code: 200,
+                    message: "Xóa thành công!",
+                });
+                break;
             default:
                 res.json({
                     code: 400,
