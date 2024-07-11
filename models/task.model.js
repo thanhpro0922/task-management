695fcd20e6968e44cfddf1d234cebaa679cdd6a6
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-11 12:22:19 +0700

    Create project task-management

---
+++ models/task.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+    {
+        title: String,
+        status: String,
+        content: String,
+        timeStart: Date,
+        timeFinish: String,
+        deleted: {
+            type: Boolean,
+            default: false,
+        },
+        deletedAt: Date,
+    },
+    { timestamps: true }
+);
+
+const Task = mongoose.model("Task", taskSchema, "tasks");
+
+module.exports = Task;
