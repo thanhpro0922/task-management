78027104a6ff3be226b3d7823d76577ddeca5b39
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-11 19:56:05 +0700

    8. Bộ lọc theo trạng thái

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -1,26 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const controller = require("../controllers/task.controller");
 
-const Task = require("../../../models/task.model");
+router.get("/", controller.index);
 
-router.get("/", async (req, res) => {
-    const tasks = await Task.find({
-        deleted: false,
-    });
-
-    res.json(tasks);
-});
-
-router.get("/detail/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const task = await Task.findOne({
-            _id: id,
-            deleted: false,
-        });
-        res.json(task);
-    } catch (error) {
-        res.json("Ko tìm thấy!");
-    }
-});
+router.get("/detail/:id", controller.detail);
 module.exports = router;
