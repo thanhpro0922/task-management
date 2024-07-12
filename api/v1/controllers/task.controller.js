57fcb2257aba6d78e250679a5ffbe6f8ca189f74
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-12 13:24:43 +0700

    7. Lấy ra danh sách task theo đúng user

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -6,6 +6,7 @@ const searchHelper = require("../../../helpers/search");
 //@ [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
     const find = {
+        $or: [{ createdBy: req.user.id }, { listUser: req.user.id }],
         deleted: false,
     };
     if (req.query.status) {
